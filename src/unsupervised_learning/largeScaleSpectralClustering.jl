7ced1bc7c07d54d6780bb40739eeaeda2fee0555
John Lapeyre <jlapeyre@users.noreply.github.com>
2018-09-25 11:04:17 +0200

    Reformat largeScaleSpectralClustering.jl

--- src/unsupervised_learning/largeScaleSpectralClustering.jl
+++ src/unsupervised_learning/largeScaleSpectralClustering.jl
@@ -1,4 +1,3 @@
-
 mutable struct LSC
     n_clusters::Int64
     n_landmarks::Int64
@@ -22,59 +21,56 @@ function LSC(;
 end
 
 function gaussianKernel(distance, bandwidth)
-    exp(-distance / (2*bandwidth^2));
+    exp(-distance / (2 * bandwidth^2));
 end
 
 function get_landmarks(X, p;method=:Kmeans)
     if(method == :Random)
-        numberOfPoints = size(X,2);
-        landmarks = X[:,randperm(numberOfPoints)[1:p]];
+        numberOfPoints = size(X, 2);
+        landmarks = X[:, randperm(numberOfPoints)[1:p]];
         return landmarks;
     end
 
     if(method == :Kmeans)
-        kmeansResult = kmeans(X,p)
+        kmeansResult = kmeans(X, p)
         return kmeansResult.centers;
     end
     throw(ArgumentError("method can only be :Kmeans or :Random"));
 end
 
 function compose_sparse_Z_hat_matrix(X, landmarks, bandwidth, r)
-
     distances = pairwise(Distances.Euclidean(), landmarks, X);
     similarities = map(x -> gaussianKernel(x, bandwidth), distances);
     ZHat = zeros(size(similarities));
 
-    for i in 1:(size(similarities,2))
-        topLandMarksIndices = partialsortperm(similarities[:,i], 1:r, rev=true);
+    for i in 1:(size(similarities, 2))
+        topLandMarksIndices = partialsortperm(similarities[:, i], 1:r, rev=true);
         topLandMarksCoefficients = similarities[topLandMarksIndices, i];
         topLandMarksCoefficients = topLandMarksCoefficients / sum(topLandMarksCoefficients);
-        ZHat[topLandMarksIndices,i] = topLandMarksCoefficients;
+        ZHat[topLandMarksIndices, i] = topLandMarksCoefficients;
     end
-    return LinearAlgebra.Matrix(LinearAlgebra.Diagonal((sum(ZHat,dims=2)[:])))^(-1/2) * ZHat;
+    return LinearAlgebra.Matrix(LinearAlgebra.Diagonal((sum(ZHat, dims=2)[:])))^(-1/2) * ZHat;
 
 end
 
-
 function train!(model::LSC, X::Matrix)
-    if size(X,1) > size(X,2)
+    if size(X, 1) > size(X, 2)
         X = copy(transpose(X))
     end
     landmarks = get_landmarks(X, model.n_landmarks, method = model.method)
     Z_hat = compose_sparse_Z_hat_matrix(X, landmarks, model.bandwidth,
                                         model.non_zero_landmarks)
     svd_result = LinearAlgebra.svd(transpose(Z_hat))
-    temp = copy(transpose(svd_result.U[:,1:model.n_clusters]))
+    temp = copy(transpose(svd_result.U[:, 1:model.n_clusters]))
     model.cluster_result = kmeans(temp, model.n_clusters).assignments
 end
 
 function plot_in_2d(model::LSC, X::Matrix)
-    if size(X,1) > size(X,2)
+    if size(X, 1) > size(X, 2)
         X = transpose(X)
     end
-    Gadfly.plot(x = X[1,:], y = X[2, :], color = model.cluster_result)
+    Gadfly.plot(x = X[1, :], y = X[2, :], color = model.cluster_result)
 end
-
 
 function test_LSC()
     path_ = "../data"
@@ -93,10 +89,10 @@ function test_LSC()
     df = DataFrame()
     for i in 1:4
         data = DelimitedFiles.readdlm(datasets[i])
-        data = transpose(convert(Array{Float64,2}, data[:,1:2]))
+        data = transpose(convert(Array{Float64, 2}, data[:, 1:2]))
         model = LSC(n_clusters = clusters[i], n_landmarks = n_landmarks[i], bandwidth = bandwidth[i])
         train!(model, copy(data))
-        dataframe = DataFrame(x=data[1,:],y=data[2,:],group=model.cluster_result,datasets=data_name[i])
+        dataframe = DataFrame(x=data[1, :], y=data[2, :], group=model.cluster_result, datasets=data_name[i])
         if count == 0
             df = dataframe
             count += 1
@@ -107,7 +103,7 @@ function test_LSC()
         #println("$(df)")
     end
     println("$(df)")
-    println("computing finied, drawing the plot......")
+    println("computing finished, drawing the plot......")
     set_default_plot_size(25cm, 14cm)
-    Gadfly.plot(df, x="x", y = "y", color = "group",xgroup = "datasets", Geom.subplot_grid(Geom.point), Guide.title("Large Scale Spectral Clustering"))
+    Gadfly.plot(df, x="x", y = "y", color = "group", xgroup = "datasets", Geom.subplot_grid(Geom.point), Guide.title("Large Scale Spectral Clustering"))
 end
